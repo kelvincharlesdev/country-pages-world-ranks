90eaa176096a1afd2a27b15c38eed4d594138825
Kelvin Charles <kelvinc1291@hotmail.com>
2024-08-22 21:04:53 -0300

    feat: Add HomePage page and start assembly with Header

---
+++ src/page/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import { Header } from '../../components';
+
+import * as S from './HomePage.styles';
+
+export const HomePage = () => {
+  return (
+    <S.Home__Wrapper>
+      <Header />
+    </S.Home__Wrapper>
+  );
+};
